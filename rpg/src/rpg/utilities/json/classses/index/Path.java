dcbc79b59d1bda7b5d3659d7efc039c05c45a7c1
Alex <longnamesrule@gmail.com>
2019-01-23 15:40:03 +0000

    adjusted index

--- rpg/src/rpg/utilities/json/classses/index/Path.java
+++ rpg/src/rpg/utilities/json/classses/index/Path.java
@@ -1,17 +1,21 @@
 package rpg.utilities.json.classses.index;
 
 public abstract class Path {
-	
+
 	//definitions
-	
+
 	private String path;
 	private String type;
-	
+    private String cla;
+
 	//getters and setters
-	
+
 	public String getPath() {return this.path;}
 	public void setPath(String path) {this.path = path;}
-	
+
 	public String getType() {return this.type;}
 	public void setType(String type) {this.type = type;}
+
+    public String getCla() {return this.cla;}
+    public Void setCla(String cla) {this.cla = cla;}
 }
