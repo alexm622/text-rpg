6dbd1219a9edd810df569390c51e50213d98b98a
Alex <acomeau62203@gmail.com>
2019-03-08 20:34:00 -0500

    tiny fixx

--- rpg/src/rpg/objects/Dialogue.java
+++ rpg/src/rpg/objects/Dialogue.java
@@ -1,49 +1,49 @@
 package rpg.objects;
 
-public class Dialogue {]
+public class Dialogue {
     private String op1;
     private String op2;
     private String op3;
     private String op4;
     private Dialogue[] results;
 
-    public String getOp1(){
+    public String getOp1() {
         return this.op1;
     }
 
-    public void setOp1(String op1){
+    public void setOp1(String op1) {
         this.op1 = op1;
     }
 
-    public String getOp2(){
+    public String getOp2() {
         return this.op2;
     }
 
-    public void setOp2(String op2){
+    public void setOp2(String op2) {
         this.op2 = op2;
     }
 
-    public String getOp3(){
+    public String getOp3() {
         return this.op3;
     }
 
-    public void setOp3(String op3){
+    public void setOp3(String op3) {
         this.op3 = op3;
     }
 
-    public String getOp4(){
+    public String getOp4() {
         return this.op4;
     }
 
-    public void setOp4(String op4){
+    public void setOp4(String op4) {
         this.op4 = op4;
     }
 
-    public Dialogue[] getResults(){
+    public Dialogue[] getResults() {
         return this.results;
     }
 
-    public void setResults(Dialogue[] results){
+    public void setResults(Dialogue[] results) {
         this.results = results;
     }
 }
