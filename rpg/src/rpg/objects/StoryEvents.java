0a9090dc50255bf1566ec599fabea34ff188b851
Alex <17982529+alexm622@users.noreply.github.com>
2019-03-18 14:04:49 -0400

    started work on test storyline and events

---
+++ rpg/src/rpg/objects/StoryEvents.java
@@ -0,0 +1,13 @@
+package rpg.objects;
+
+public class StoryEvents{
+    private StoryEvent[] events;
+    
+    public StoryEvent[] getEvents(){
+        return this.events;
+    }
+
+    public void setStoryEvents(StoryEvent[] events){
+        this.events = events;
+    }
+}
