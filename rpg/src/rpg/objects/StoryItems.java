863183d7ef2b06809ee09915a62c2118d2d8b277
Alex <17982529+alexm622@users.noreply.github.com>
2019-03-19 10:10:17 -0400

    some work on test story line

---
+++ rpg/src/rpg/objects/StoryItems.java
@@ -0,0 +1,14 @@
+package rpg.objects;
+
+public class StoryItems {
+    private Item items[];
+
+    public Item[] getItems(){
+        return this.items;
+    }
+
+    public void setItems(Item[] items){
+        this.items = items;
+    }
+
+}
