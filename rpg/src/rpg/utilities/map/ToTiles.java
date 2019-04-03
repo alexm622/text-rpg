ab0f5260c4d29b9692d3e0f1e7e28f4c6aa46e9e
Alex <acomeau62203@gmail.com>
2019-04-03 14:40:08 -0400

    did some work on double to tiles.

--- rpg/src/rpg/utilities/map/ToTiles.java
+++ rpg/src/rpg/utilities/map/ToTiles.java
@@ -7,37 +7,46 @@ public class ToTiles {
 
     private double[][] doub;
     private Tile[][] tiles;
+    private Tile[] set;
 
     public ToTiles(Handler h){
 
         doub = h.getG().getMemory().getMem().getMap().getTiles();
-        tiles = new Tile[doub.length][doub[0].length];
+        set = h.getG().getMemory().getMem().getTileSet().getTiles();
+        tiles = new Tile[doub.length-1][doub[0].length-1];
 
         //-7.5, -5.0 , -2.5, 0, 2.5, 5.0, 7.5
         for(int i = 0; i < doub.length; i++){
             for(int x = 0; x < doub[i].length; x++){
-                String temp = Double.toString(doub[i][x]);
+                String temp = Double.toStgetTilesring(doub[i][x]);
                 switch (temp){
 
                     case "-7.5":
+	                    tiles[i][x] = set[0];
                         break;
 
                     case "-5.0":
+	                    tiles[i][x] = set[1];
                         break;
 
                     case "-2.5":
+	                    tiles[i][x] = set[2];
                         break;
 
                     case "0":
+	                    tiles[i][x] = set[3];
                         break;
 
                     case "2.5":
+	                    tiles[i][x] = set[4];
                         break;
 
                     case "5.0":
+	                    tiles[i][x] = set[5];
                         break;
 
                     case "7.5":
+	                    tiles[i][x] = set[6];
                         break;
                     
                     default:
