c2809012e9e48f2ffa60e7317737659d1231c8ae
Alex <17982529+alexm622@users.noreply.github.com>
2019-05-01 14:07:18 -0400

    some comments

--- rpg/src/rpg/guis/draw/DrawMap.java
+++ rpg/src/rpg/guis/draw/DrawMap.java
@@ -37,10 +37,16 @@ public class DrawMap {
     }
 
     private void draw(){
+
+        //get the height and width of the map
         int width, height;
         width = m.getMap()[0].length;
         height = m.getMap().length;
+
+        //calculate center
         int[] center = new int[]{(width - width%2)/2 + width%2, (height - height%2)/2 + height%2};
+
+        //find the array entry of the center tile in the map as tiles
 
         int[] relPos = {pos[0] + center[0], pos[1] + center[1]};
         relPos[0] -= 1;
@@ -48,6 +54,9 @@ public class DrawMap {
 
         Tile[][] draw;
         draw = new Tile[3][3];
+
+        //get the seen array of tiles
+
         for(int i = 0; i < 4; i++){
             for(int j = 0; i < 4; i++){
                 draw[i][j] = m.getMap()[relPos[0] - (i-2)][j + relPos[1]];
